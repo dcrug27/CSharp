926a98435a3d0ad12bfabd33224cdc4d529186a8
dcrug27 <102136422+dcrug27@users.noreply.github.com>
2022-11-30 17:25:24 +0300

    Delete Seminar01 directory

--- Seminar01/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿//Вывести квадрат числа
-
-Console.WriteLine("start"); //Начало
-
-int number = 13; // Определили целое число
-int result = number * number; //Подсчет квадрата числа
-Console.WriteLine(result); //Вывели результат подсчета квадрата числа
-
-Console.WriteLine("end"); //Конец
