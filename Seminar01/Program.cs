851eebd66748778c098920450629c96e114c3b45
Vadim <vadyaburr@gmail.com>
2022-11-30 17:22:16 +0300

    Задача на нахождение квадрата числа

---
+++ Seminar01/Program.cs
@@ -0,0 +1,9 @@
+﻿//Вывести квадрат числа
+
+Console.WriteLine("start"); //Начало
+
+int number = 13; // Определили целое число
+int result = number * number; //Подсчет квадрата числа
+Console.WriteLine(result); //Вывели результат подсчета квадрата числа
+
+Console.WriteLine("end"); //Конец
