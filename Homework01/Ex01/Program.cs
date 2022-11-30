ed4ae7a9708eaee6d10effc3b0b6d03336402e54
Vadim <vadyaburr@gmail.com>
2022-11-30 18:35:56 +0300

    Hw01 Ex01 - нахождение максимального числа из 3-ех чисел

---
+++ Homework01/Ex01/Program.cs
@@ -0,0 +1,25 @@
+﻿//
+
+int a = 2;
+int b = 3;
+int c = 7;
+
+int max = 0;
+
+if (a > b && a > c)
+{
+    max = a;
+}
+else
+{
+    if (b > c)
+    {
+        max = b;
+    }
+    else
+    {
+        max = c;
+    }
+}
+
+Console.WriteLine(max);
