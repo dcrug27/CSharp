58fa315988f0eca6630069434cb3211983f27fe8
Vadim <vadyaburr@gmail.com>
2022-12-09 00:22:56 +0300

    Hw03 Ex01 - Является ли число палиндромом

---
+++ Homework03/Ex01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int temp = number;
+int rev = 0;
+
+while (number > 0)
+{
+    int dig = number % 10;
+    rev = rev * 10 + dig;
+    number = number / 10;
+}
+if (temp == rev)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");    
+}
