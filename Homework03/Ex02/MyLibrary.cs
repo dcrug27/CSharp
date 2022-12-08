7d79646af95b5b4f85663d11cab9f8cc5091afe9
Vadim <vadyaburr@gmail.com>
2022-12-09 00:41:06 +0300

    Hw03 Ex02 - Расстояние между двумя точками

---
+++ Homework03/Ex02/MyLibrary.cs
@@ -0,0 +1,8 @@
+public static class MyLibrary
+{
+    public static int Input (string text)
+    {
+        Console.Write(text);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+}
---
+++ Homework03/Ex02/Program.cs
@@ -0,0 +1,14 @@
+﻿using static MyLibrary;
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+int x1 = Input("Введите x1: ");
+int y1 = Input("Введите y1: ");
+int x2 = Input("Введите x2: ");
+int y2 = Input("Введите y2: ");
+
+int x3 = x2 - x1;
+int y3 = y2 - y1;
+
+double distance = Math.Sqrt(Math.Pow(x3, 2) + Math.Pow(y3, 2));
+
+Console.WriteLine(Math.Round(distance, 1));
