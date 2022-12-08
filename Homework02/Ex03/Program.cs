09dcceba9a00604426676416fedb6e978681e5c0
Vadim <vadyaburr@gmail.com>
2022-12-09 00:07:11 +0300

    Hw02 Ex03 - Проверка на выходной

---
+++ Homework02/Ex03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите цифру обозначающую день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0 && number < 8)
+{
+    if (number < 6)
+    {
+        Console.WriteLine("Нет");
+    }
+    else
+    {
+        Console.WriteLine("Да");
+    }
+}
+else
+{
+    Console.WriteLine("Введите число от 1 до 7");
+}
