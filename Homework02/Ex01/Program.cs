4fca82f598286031354b1b483035008238054051
Vadim <vadyaburr@gmail.com>
2022-12-08 23:40:53 +0300

    Hw02 Ex01 - Вторая цифра трехзначного числа

---
+++ Homework02/Ex01/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+number = (number / 10) % 10;
+
+Console.WriteLine(number);
