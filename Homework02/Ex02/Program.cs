745f0e6452bb825b508fd68db9642a5b93b69b7f
Vadim <vadyaburr@gmail.com>
2022-12-08 23:58:57 +0300

    Hw02 Ex02 - третья цифру заданного числа

---
+++ Homework02/Ex02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99)
+{
+    double count = Math.Log10(number);
+    for (int i = 0; i < count - 3; i++)
+    {
+        int a = number / 10;
+        number = a;
+    }
+    number = number % 10;
+    Console.WriteLine(number);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
