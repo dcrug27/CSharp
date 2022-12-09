2b6b7b70cb32a55fe59b8a7cc0f8af00837fca23
Vadim <vadyaburr@gmail.com>
2022-12-10 01:02:59 +0300

    Ln03

---
+++ Lesson03/Ex01/Program.cs
@@ -0,0 +1,63 @@
+﻿//Вид 1
+void Method1()
+{
+    Console.WriteLine("Author Vadim Matveev");
+}
+Method1();// Вызвать метод 
+
+//Вид 2
+void Method2(string msg)
+{
+    System.Console.WriteLine(msg);
+}
+Method2(msg: "Text message");
+
+//Вид 2.1
+void Method2_1(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method2_1("Text message", 4); // Method2_1(msg: "text", count: 4) - Можно так.
+
+//Вид 3
+int Method3();
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+//Вид 4
+string Method4(int count, char text);
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "qwerty");
+Console.WriteLine(res);
+
+//Вид 4.1
+
+string Method4_1(int count, char text);
+{
+    int i = 0;
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4_1(10, "qwerty");
+Console.WriteLine(res);
---
+++ Lesson03/Ex04/Program.cs
@@ -0,0 +1,34 @@
+﻿//Упорядочить массив
+
+int[] arr = { 1, 5, 4, 3, 6, 7, 2, 8, 6, 1, 7, 9 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length - 1; j++)
+        {
+            if(array[j]< array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
