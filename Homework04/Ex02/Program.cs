ffade138f5db80c5cc3b39038d8c172fb59812b3
Vadim <vadyaburr@gmail.com>
2022-12-11 12:47:34 +0300

    Hw04

---
+++ Homework04/Ex02/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+//Строки использовать нельзя
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+
+double count = Math.Log10(number);
+    for (int i = 0; i < count ; i++)
+    {
+        int a = number / 10;
+        int b = number % 10;
+        number = a;
+        result = result + b;
+    }
+System.Console.WriteLine(result);
---
+++ Homework04/Ex03/Program.cs
@@ -0,0 +1,37 @@
+﻿void FillArray(int[] zero) //Заполнение массива
+{
+    for (int i = 0; i < zero.Length; i++)
+    {
+        zero[i] = new Random().Next(0, 15);
+    }
+}
+
+void PrintArray(int[] one) //Вывод массива
+{
+    for (int i = 0; i < one.Length; i++)
+    {
+        Console.Write(one[i] + " ");
+    }
+}
+
+//Напишите программу, которая задаёт массив из N элементов (из диапазона [0-14] ) и выводит на экран десятичное представление числа, записанного в СС по основанию 15
+//N: 3 [9, 4, 12] => 2097
+
+Console.Write("Кол-во элементов в массиве: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[N];
+
+FillArray(array);
+PrintArray(array);
+int powwe = array.Length - 1;
+double output = 0;
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    double result = array[i] * Math.Pow(15, powwe);
+    powwe--;
+    output = output + result;
+}
+Console.Write($"-> {output}");
